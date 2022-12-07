f1bc1f939689453647cd49a17263cca56c234301
dr-azizbek <115967245+dr-azizbek@users.noreply.github.com>
2022-12-07 08:23:11 +0500

    eror

--- NTga kelish kunlari/script.js
+++ NTga kelish kunlari/script.js
@@ -3,7 +3,7 @@
 function weekss() {
     let weeks = prompt("Haftani kiriting:")
     let group = +prompt("Guruhni kiriting(n-guruh):")
-    if (weeks == "Dushanba" || weeks == "dushanba" || weeks == "Chorshanba" || weeks == "chorshanba" || weeks == "Juma" || weeks == "juma" && group == "79-guruh") {
+    if ((weeks == "Dushanba" || weeks == "dushanba" || weeks == "Chorshanba" || weeks == "chorshanba" || weeks == "Juma" || weeks == "juma") && group == "79-guruh") {
         result = "Sizning darsga keladigan kuninggiz ekan."
     }
     else{ 
